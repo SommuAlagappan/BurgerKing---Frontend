0d2680778587ec736a53142cc3c0e0181340894f
SommuAlagappan <sommualagappanm.1001@gmail.com>
2023-01-30 21:43:30 +0530

    first commit

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const env = {
-    api: "http://localhost:4001",
-    KEY :"rzp_test_OrIhFPdRWa9OVH",
-    SECRET : "3EXnvyojZ8oJ8dXRglsejRJp"
+    api: "https://burgerking.onrender.com",
+    KEY: "rzp_test_OrIhFPdRWa9OVH",
+    SECRET: "3EXnvyojZ8oJ8dXRglsejRJp"
 
 }
