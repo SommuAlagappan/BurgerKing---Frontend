022b9fb9d634cb7189e9fde1e943c000dd6647f8
SommuAlagappan <sommualagappanm.1001@gmail.com>
2023-02-04 13:30:22 +0530

    c

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -3,11 +3,12 @@ import { Outlet } from "react-router-dom";
 import Login from "./Login";
 
 
-
 function ProtectedRoutes() {
   return (
-    window.localStorage.apptoken.length>70?<Outlet/> : <Login/>
+    window.localStorage.length>0?<Outlet/> :<Login/>
     
   )
 }
+
 export default ProtectedRoutes
+
