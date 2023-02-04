d46866c2047fd0f8dbf3cadee795791bb3eeb2f6
SommuAlagappan <sommualagappanm.1001@gmail.com>
2023-02-04 13:44:18 +0530

    commit

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -5,12 +5,12 @@ import Login from "./Login";
 
 function ProtectedRoutes() {
   return (
-    window.localStorage.apptoken.length>0?<Outlet/> :<Login/>
+    window.localStorage.length>70?<Outlet/> :<Login/>
     
   )
 }
 
 export default ProtectedRoutes
 
-console.log(window.localStorage.apptoken.length>0)
+console.log(window.localStorage.length>70)
 
